77da9f0707ae98740d1a7c3e9ad240a959b3b87d
Evan Loshin <loshinevan@gmail.com>
2018-08-18 23:27:14 -0700

    Initial project commit

---
+++ functions.py
@@ -0,0 +1,85 @@
+# import python libraries
+import glob
+import math
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+# import project dependencies
+from classes import undistorter, transformer, laneFinder
+
+### grab images from a directory
+### returns np array of images
+def read_image_dir(path):
+    Images = np.array([plt.imread(file) for file in glob.glob(path + '*')])
+    return Images
+
+### apply color thresholds to image
+### returns binarized image
+def binarize(img, sobelx_thresh, sobely_thresh, saturation_thresh, value_thresh):
+    # grab S channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:, :, 2]
+    # grab v channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    v_channel = hsv[:, :, 2]
+    # grab grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # take derivative of the gradient w.r.t. X
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    # take derivative of the gradient w.r.t. Y
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    # take absolute value of the derivatives
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    # rescale back to 255
+    scaled_sobelx = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
+    scaled_sobely = np.uint8(255 * abs_sobely / np.max(abs_sobely))
+    # translate pixels in each filtered image to binary values
+    sobelx_binary = np.zeros_like(scaled_sobelx)
+    sobely_binary = np.zeros_like(scaled_sobely)
+    sobelx_binary[(scaled_sobelx >= sobelx_thresh[0]) & (scaled_sobelx <= sobelx_thresh[1])] = 1
+    sobely_binary[(scaled_sobely >= sobely_thresh[0]) & (scaled_sobely >= sobely_thresh[1])] = 1
+    saturation_binary = np.zeros_like(s_channel)
+    saturation_binary[(s_channel >= saturation_thresh[0]) & (s_channel <= saturation_thresh[1])] = 1
+    value_binary = np.zeros_like(v_channel)
+    value_binary[(v_channel >= value_thresh[0]) & (v_channel <= value_thresh[1])] = 1
+    # combine binarized images
+    combined = np.zeros_like(saturation_binary)
+    combined[((sobelx_binary == 1) | (sobely_binary == 1)) | ((saturation_binary == 1) & (value_binary == 1))] = 1
+    return combined
+
+### find center of each lane at bottom of frame
+### no return
+def find_center_points(binary_img):
+    # grab bottom half of image
+    bottom = binary_img[binary_img.shape[0] // 2:, :]
+    # take histogram of activated pixel amplitude
+    hist = np.sum(bottom, axis=0)
+    # find lane centers
+    mid = np.int(hist.shape[0] // 2)
+    left_center = np.argmax(hist[:mid])
+    right_center = np.argmax(hist[mid:]) + mid
+    return [left_center, right_center]
+
+### process frames of video one by one
+### returns processed image
+def video_pipeline(frame, undist, txf, thresholds, lanes, params):
+    undistorted = undist.undistort(frame)
+    warped = txf.warp(undistorted)
+    binarized = binarize(warped, thresholds[0], thresholds[1], thresholds[2], thresholds[3])
+    detected = lanes.find_lanes(binarized, params[0], params[1], params[2], params[3], params[4], params[5])
+    result = lanes.project_lines(detected, undistorted, txf)
+    # Overlay visualization of lane finding function
+    detected_resized = cv2.resize(detected, (0, 0), fx=0.35, fy=0.35)
+    y_pos = 20
+    x_pos = 20
+    result[y_pos: y_pos + detected_resized.shape[0], x_pos: x_pos + detected_resized.shape[1]] = detected_resized
+    # Overlay radius measurements
+    radius = lanes.get_radius()
+    text = 'Radius of curvature: ' + str(int(radius)) + 'm'
+    cv2.putText(result, text, (550, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), thickness=3)
+    # Overlay center distance
+    distance = lanes.get_center_distance(warped)
+    text2 = 'Distance from center: ' + str(round(distance, 1)) + 'm'
+    cv2.putText(result, text2, (550, 120), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), thickness=3)
+    return result
---
+++ main.py
@@ -0,0 +1,76 @@
+# import python libraries
+print('Importing dependencies...')
+import glob
+import math
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+# import project dependencies
+import functions as fn
+from classes import undistorter, transformer, laneFinder
+
+
+def main():
+
+    ######################## HYPERPARAMETERS ########################
+    nx_corners = 9  # number of chessboard corners in horizontal direction
+    ny_corners = 6  # number of chessboard corners in vertical direction
+    x_offset = 400  # horizontal offset for perspective transformation
+    y_offset = -10  # vertical offset for perspective transformation
+    sobelx_thresh = [20, 255]  # pixel thresholds for horizontal color gradient
+    sobely_thresh = [35, 255]  # pixel thresholds for vertical color gradient
+    saturation_thresh = [60, 255]  # pixel thresholds for saturation channel in HLS color space
+    value_thresh = [40, 255]  # pixel thresholds for value channel in HSV color space
+    n_windows = 9  # number of cross-sliding windows
+    window_width = 150  # width (pixels) of cross-sliding windows
+    min_pix = 40  # minimum number of activated pixels required to recenter window
+    curve_margin = 50  # margin around last polynomial fit for including new lane pixels
+    smooth_factor = 8  # moving average period for last n polynomial fits
+    max_diff = 50  # allowable percent difference between new and previous coefficients
+    ###################### END HYPERPARAMETERS ######################
+
+    # package up hyperparameters for passing to video pipeline
+    hyperparameters = [n_windows, window_width, min_pix, curve_margin, smooth_factor, max_diff]
+
+    # grab example driving images and calibration chessboard images
+    print('Calibrating camera...')
+    Examples = fn.read_image_dir('test_images/')
+    Chessboards = fn.read_image_dir('camera_cal/')
+
+    # measure camera distortion
+    undist = undistorter(Chessboards, nx_corners, ny_corners)
+
+    # create perspective transformation matrix
+    print('Creating detection pipeline...')
+    cal = undist.undistort(np.copy(Examples[0]))
+    mask = np.int32([[[597, 450], [680, 450], [1020, 675], [290, 675]]])
+    txf = transformer(cal, mask, x_offset, y_offset)
+    txf.create_transformation()
+    #txf.plot_mask() # visual aid for determining coordinates
+
+    # undistort and warp perspective
+    undistorted = undist.undistort(cal)
+    warped = txf.warp(undistorted)
+    #plt.imsave('temp.png', warped)
+
+    # create a binarized image to filter out noise
+    thresholds = [sobelx_thresh, sobely_thresh, saturation_thresh, value_thresh]
+    binarized = fn.binarize(warped, sobelx_thresh, sobely_thresh, saturation_thresh, value_thresh)
+
+    # pixel to meter conversion
+    lanes = laneFinder()
+    lanes.generate_unit_conversion(binarized)
+
+    # generate video
+    output_file = 'output_videos/output_project_video.mp4'
+    input_file = 'test_videos/project_video.mp4'
+    clip = VideoFileClip(input_file)#.subclip(5, 12)
+    result = clip.fl_image(lambda frame: fn.video_pipeline(frame, undist, txf, thresholds, lanes, hyperparameters))
+    result.write_videofile(output_file, audio=False)
+
+    # result = fn.video_pipeline(Examples[0], undist, txf, thresholds, lanes, hyperparameters)
+    # plt.imsave('test.png', result)
+
+if __name__ == '__main__': main()
